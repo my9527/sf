ae3fc7caee2db7cdc6a58e5c2e3be39e7ecc5c0c
yang <yang.yang@kucoinc.com>
2022-01-11 14:28:04 +0800

    mod

---
+++ precache-manifest.c25ae1b96b6abe3db871ba5bd8b33506.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb794c29a6d90b069c7b71e932c4fafd",
+    "url": "/sunflower-front/index.html"
+  },
+  {
+    "revision": "0fb2d38396078699dfa2",
+    "url": "/sunflower-front/static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "7b657f90ee2778113211",
+    "url": "/sunflower-front/static/css/main.ac191864.chunk.css"
+  },
+  {
+    "revision": "0fb2d38396078699dfa2",
+    "url": "/sunflower-front/static/js/2.e561e8c8.chunk.js"
+  },
+  {
+    "revision": "dbd161b9ac48e6187d724e7ff9d3abb2",
+    "url": "/sunflower-front/static/js/2.e561e8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b657f90ee2778113211",
+    "url": "/sunflower-front/static/js/main.34bec72c.chunk.js"
+  },
+  {
+    "revision": "796f2c53002a45c719c2",
+    "url": "/sunflower-front/static/js/runtime-main.ce656015.js"
+  },
+  {
+    "revision": "51c1242870ecfc1e74411c95c4d89904",
+    "url": "/sunflower-front/static/media/logo.51c12428.png"
+  },
+  {
+    "revision": "afdb2d67b2cfa0dad4d27d0b71728a04",
+    "url": "/sunflower-front/static/media/speed-up.afdb2d67.png"
+  },
+  {
+    "revision": "9a61915e7b3cdcf7f844ce053c67101d",
+    "url": "/sunflower-front/static/media/spinning_coin.9a61915e.gif"
+  },
+  {
+    "revision": "70fb56907514e7f4469ead631eba45c9",
+    "url": "/sunflower-front/static/media/willow_tree.70fb5690.mp3"
+  }
+]);
